bd0d480e1997e29fbf02e1ccb6325c1dab1a7daa
LandyKing <landykingdom@gmail.com>
2018-08-08 22:01:15 +0800

    补充测试用例

--- link-core/src/test/java/com/github/landyking/link/execution/DbInsertTest.java
+++ link-core/src/test/java/com/github/landyking/link/execution/DbInsertTest.java
@@ -1,11 +1,28 @@
 package com.github.landyking.link.execution;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import com.github.landyking.link.DirectiveMojo;
+import com.github.landyking.link.TestH2Database;
+import com.github.landyking.link.pot.EmptyInputPot;
+import com.github.landyking.link.spel.SpelTool;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author: landy
  * @date: 2018-08-08 21:28
  */
-public class DbInsertTest {
+public class DbInsertTest extends TestH2Database {
+    @Test
+    public void testSuccess() throws Exception {
+        EmptyInputPot pot = new EmptyInputPot();
+        pot.put("deptNo", "d555");
+        DirectiveMojo mojo = getDm().callDirective("test.execution.dbSelect", pot);
+        Assert.assertEquals(null, SpelTool.getValueFromExpress(mojo.getAfterOutput(), "#root[rst1].finalValue"));
+        pot.put("deptName", "helloworld");
+        mojo = getDm().callDirective("test.execution.dbInsert", pot);
+        Assert.assertEquals(true, SpelTool.getValueFromExpress(mojo.getAfterOutput(), "#root[success].finalValue"));
+        mojo = getDm().callDirective("test.execution.dbSelect", pot);
+        Assert.assertEquals("helloworld", SpelTool.getValueFromExpress(mojo.getAfterOutput(), "#root[rst1].finalValue"));
 
+    }
 }
