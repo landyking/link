c22ad4ef452222cc2688818d8778fcf6098af4c8
LandyKing <landykingdom@gmail.com>
2018-08-23 15:53:28 +0800

    补充测试用例

---
+++ link-core/src/test/java/com/github/landyking/link/spel/SpelBeanResolverTest.java
@@ -0,0 +1,20 @@
+package com.github.landyking.link.spel;
+
+import com.github.landyking.link.DirectiveMojo;
+import com.github.landyking.link.TestH2Database;
+import com.github.landyking.link.pot.EmptyInputPot;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author: landy
+ * @date: 2018-08-23 15:41
+ */
+public class SpelBeanResolverTest extends TestH2Database {
+    @Test
+    public void test() throws Exception {
+        EmptyInputPot pot = new EmptyInputPot();
+        DirectiveMojo mojo = getDm().callDirective("test.execution.SpelBeanResolverTest", pot);
+        Assert.assertEquals("hello", SpelTool.getValueFromExpress(mojo.getAfterOutput(), "#root[rst1].finalValue"));
+    }
+}
---
+++ link-core/src/test/java/com/github/landyking/link/spel/Tool.java
@@ -0,0 +1,11 @@
+package com.github.landyking.link.spel;
+
+/**
+ * @author: landy
+ * @date: 2018-08-23 15:39
+ */
+public class Tool {
+    public String hello() {
+        return "hello";
+    }
+}
